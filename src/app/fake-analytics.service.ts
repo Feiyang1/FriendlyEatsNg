e888b23068b164a76f7e47727fb674dfee0ae097
Feiyang1 <plane1113@gmail.com>
2020-10-11 22:28:48 -0700

    persist analytics state

--- src/app/fake-analytics.service.ts
+++ src/app/fake-analytics.service.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@angular/core';
+import { PersistenceService } from './persistence.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FakeAnalyticsService {
   private _wroteReview = false;
-  constructor() { }
+  constructor(private persistenceService: PersistenceService) {
+    this.init();
+  }
 
-  get wroteReview () {
+  private async init() {
+    const analyticsState = await this.persistenceService.getAnalyticsState();
+
+    if (analyticsState) {
+      this._wroteReview = analyticsState.wroteReview;
+    }
+  }
+
+  get wroteReview() {
     return this._wroteReview;
   }
 
   set wroteReview(wrote: boolean) {
     this._wroteReview = wrote;
+    this.save();
+  }
+
+  private save() {
+    return this.persistenceService.setAnalyticsState({
+      wroteReview: this._wroteReview
+    });
   }
 }
 
--- src/app/messages.service.ts
+++ src/app/messages.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { Message } from '../models/message';
 import { Audience, FakeAnalyticsService } from './fake-analytics.service';
+import { PersistenceService } from './persistence.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,17 @@ export class MessagesService {
   private _messages = new Subject<Message>();
   public readonly messages: Observable<Message> = this._messages.asObservable();
 
-  constructor(private _analyticsService: FakeAnalyticsService) { }
+  constructor(private _analyticsService: FakeAnalyticsService, private persistenceService: PersistenceService) {
+    this.init();
+  }
+
+  private async init() {
+    const fcmState = await this.persistenceService.getFCMState();
+
+    if (fcmState) {
+      this._messagesHistory = fcmState.messages;
+    }
+  }
 
   send(msg: Message) {
     this._messages.next(msg);
@@ -23,6 +34,8 @@ export class MessagesService {
       || (msg.target.audience === Audience.Reviewers && this._analyticsService.wroteReview)) {
       this._unreadMessages = [...this._unreadMessages, msg];
     }
+
+    this.save();
   }
 
   resetUnreadMessage() {
@@ -35,5 +48,11 @@ export class MessagesService {
 
   get unreadMessage() {
     return this._unreadMessages;
+  }
+
+  private save() {
+    return this.persistenceService.setFCMState({
+      messages: this._messagesHistory
+    });
   }
 }
